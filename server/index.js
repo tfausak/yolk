c8bed0a77263010bfdc63d2c1a3f2690066fbab7
Taylor Fausak <taylor@fausak.me>
2019-09-08 10:30:11 -0400

    Parse JSON output from GHCi

--- server/index.js
+++ server/index.js
@@ -12,28 +12,68 @@ const ghci = childProcess.spawn(
     '--',
     'ghci',
     '-ddump-json',
-    '-fwrite-ide-info',
-    '-Weverything',
   ]);
 
+toSeverity = (severity) => {
+  switch (severity) {
+    case 'SevError': return vscode.DiagnosticSeverity.Error;
+    case 'SevWarning': return vscode.DiagnosticSeverity.Warning;
+    default: return vscode.DiagnosticSeverity.Information;
+  }
+};
+
+let buffer = "";
 ghci.stdout.on('data', (data) => {
-  connection.console.log(`ghci stdout: ${data}`);
+  buffer += data.toString();
+  const index = buffer.indexOf('\n');
+  if (index !== -1) {
+    const line = buffer.substring(0, index);
+    buffer = buffer.substring(index + 1);
+    try {
+      const json = JSON.parse(line);
+      connection.console.info(JSON.stringify(json));
+      if (json.span) {
+        connection.sendDiagnostics({
+          diagnostics: [
+            {
+              code: json.reason,
+              message: json.doc,
+              range: {
+                end: {
+                  character: json.span.endCol,
+                  line: json.span.endLine - 1,
+                },
+                start: {
+                  character: json.span.startCol,
+                  line: json.span.startLine - 1,
+                },
+              },
+              severity: toSeverity(json.severity),
+              source: 'ghc',
+            }
+          ],
+          uri: pathToUri(json.span.file),
+        });
+      }
+    } catch (err) {
+      connection.console.warn(line);
+    }
+  }
 });
 
 ghci.stderr.on('data', (data) => {
-  connection.console.log(`ghci stderr: ${data}`);
+  connection.console.warn(data.toString().trimEnd());
 });
 
 ghci.on('close', (code) => {
-  connection.console.log(`ghci close: ${code}`);
+  connection.console.warn(`ghci closed with code ${code}`);
 });
 
 [
-  ':set prompt ">>> "',
-  ':set prompt-cont "... "',
+  ':set prompt ""',
+  ':set prompt-cont ""',
   ':set +c',
 ].forEach((command) => {
-  connection.console.log(`ghci stdin: ${command}`);
   ghci.stdin.write(`${command}\n`, (err) => {
     if (err) {
       throw err;
@@ -42,58 +82,41 @@ ghci.on('close', (code) => {
 });
 
 connection.onInitialize(() => {
-  connection.console.log('onInitialize');
+  connection.console.info('onInitialize');
   return {
     capabilities: {
       textDocumentSync: {
         openClose: true,
+        save: true,
       },
     },
   };
 });
 
-connection.onDidOpenTextDocument((params) => {
-  // This should really use url.fileURLToPath, but VSCode uses Node 10.11.0 and
-  // that function was added in 10.12.0.
-  // <https://nodejs.org/docs/v10.12.0/api/url.html#url_url_fileurltopath_url>
-  const file = decodeURIComponent(params.textDocument.uri)
-    .replace(/file:\/\/\/([a-z]):\//i, '$1:/');
+// This should really use url.fileURLToPath, but VSCode uses Node 10.11.0 and
+// that function was added in 10.12.0.
+// <https://nodejs.org/docs/v10.12.0/api/url.html#url_url_fileurltopath_url>
+const uriToPath = (uri) => decodeURIComponent(uri)
+  .replace(/file:\/\/\/([a-z]):\//i, '$1:/');
 
-  connection.console.log(`onDidOpenTextDocument ${file}`);
+const pathToUri = (path) => `file:///${encodeURIComponent(path)}`;
+
+connection.onDidOpenTextDocument((params) => {
+  const file = uriToPath(params.textDocument.uri);
+  connection.console.info(`onDidOpenTextDocument ${file}`);
+  ghci.stdin.write(`:load ${file}\n`, (err) => { if (err) { throw err; } });
+});
+
+connection.onDidSaveTextDocument((params) => {
+  const file = uriToPath(params.textDocument.uri);
+  connection.console.info(`onDidSaveTextDocument ${file}`);
+  connection.sendDiagnostics({ diagnostics: [], uri: params.textDocument.uri });
   ghci.stdin.write(`:load ${file}\n`, (err) => { if (err) { throw err; } });
 });
 
 connection.onDidCloseTextDocument((params) => {
-  connection.console.log(`onDidCloseTextDocument ${params.textDocument.uri}`);
+  connection.console.info(`onDidCloseTextDocument ${params.textDocument.uri}`);
 });
 
 connection.listen();
 connection.console.error('Hello from Yolk!');
-
-// example diagnostic
-/*
-connection.onDidChangeTextDocument((params) => {
-  log(`onDidChangeTextDocument: ${params.textDocument.uri}`);
-  const diagnostics = [];
-  if (Math.random() < 0.5) {
-    log('adding sample diagnostic');
-    diagnostics.push({
-      code: 'yolk-diagnostic-code',
-      message: 'yolk-diagnostic-message',
-      range: {
-        end: {
-          character: 1,
-          line: 0,
-        },
-        start: {
-          character: 0,
-          line: 0,
-        },
-      },
-      severity: vscode.DiagnosticSeverity.Hint,
-      source: 'yolk-diagnostic-source',
-    });
-  }
-  connection.sendDiagnostics({ diagnostics, uri: params.textDocument.uri });
-});
-*/
